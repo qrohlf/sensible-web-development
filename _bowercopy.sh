1a48ef74acc9dcf139c6dc1dee72ae112196729d
Quinn Rohlf <qr@qrohlf.com>
2014-04-30 17:15:07 -0700

    concat scripts and serve them all from scripts.js

--- _bowercopy.sh
+++ _bowercopy.sh
@@ -7,3 +7,14 @@ cp -f bower_components/trianglify/trianglify.min.js _includes
 
 # glyphicons
 cp -f bower_components/bootstrap-sass-official/vendor/assets/fonts/bootstrap/* fonts
+
+# jquery
+cp -f bower_components/jquery/dist/jquery.min.js _includes
+cp -f bower_components/jquery/dist/jquery.js js
+cp -f bower_components/jquery/dist/jquery.min.map js
+
+# bootstrap
+cp -f bower_components/bootstrap/dist/js/bootstrap.min.js _includes
+
+# d3 
+cp -f bower_components/d3/d3.min.js _includes
--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
 ---
 ---
+{% include jquery.min.js %}
+{% include bootstrap.min.js %}
 {% include trianglify.min.js %}
+{% include d3.min.js %}
 {% include app.js %}
 {% include analytics.js %}
